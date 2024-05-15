83ba359b7e797b8f8c748d73ecf494cd3030acf7
S V J S N S L P KHOWSHIKK SHARMA <116909655+Khowshikksharma@users.noreply.github.com>
2024-05-15 15:13:29 +0530

    V1.0

---
+++ script.js
@@ -0,0 +1,110 @@
+const playButton=document.getElementsByClassName("play")[0]
+const lapButton=document.getElementsByClassName("lap")[0]
+const resetButton=document.getElementsByClassName("reset")[0]
+const clearButton=document.getElementsByClassName("lap-clear-button")[0]
+const minute=document.getElementsByClassName("minute")[0]
+const secound=document.getElementsByClassName("sec")[0]
+const centiSecound=document.getElementsByClassName("msec")[0]
+const laps=document.getElementsByClassName("laps")[0]
+const bg=document.getElementsByClassName("outer-circle")[0]
+
+
+let isPlay=false
+let secCounter=0
+let sec
+let centiCounter=0
+let centiSec
+let minCounter=0
+let min
+let isReset=false
+let lapItem=0
+
+const tooggleButton = ()=>{
+    lapButton.classList.remove("hidden")
+    resetButton.classList.remove("hidden")
+}
+
+const play = ()=> {
+    if(!isPlay && !isReset){
+        
+        playButton.innerHTML='Pause'
+        bg.classList.add("animation-bg")
+        min = setInterval(()=>{
+            if(minCounter===60){
+                minCounter=0
+            }
+            minute.innerHTML=`${++minCounter} :`
+        },60*1000);
+        sec = setInterval(()=>{
+            if(secCounter===60){
+                secCounter=0
+            }
+            secound.innerHTML=`&nbsp;${++secCounter} :`
+        },1000);
+        centiSec = setInterval(()=>{
+            if(centiCounter===100){
+                centiCounter=0
+            }
+            centiSecound.innerHTML=`&nbsp;${++centiCounter} `
+        },10);
+        isPlay=true
+        isReset=true
+       
+    }else{
+
+        playButton.innerHTML='Play'
+        clearInterval(min);
+        clearInterval(sec);
+        clearInterval(centiSec);
+        bg.classList.remove("animation-bg")
+        isPlay=false
+        isReset=false
+
+        
+    }
+    tooggleButton();
+}
+
+
+const reset = () =>{
+    isReset=true
+    // location.reload()
+    centiCounter=0
+    secCounter=0
+    minCounter=0
+    play()
+    lapButton.classList.add("hidden")
+    resetButton.classList.add("hidden")
+    secound.innerHTML='&nbsp;0 :'
+    centiSecound.innerHTML='&nbsp;0'
+    minute.innerHTML='&nbsp;0 :'
+}
+
+const lap=()=>{
+    const li=document.createElement("li")
+    const number=document.createElement("span")
+    const timestamp=document.createElement("span")
+
+    li.setAttribute("class","lap-item")
+    number.setAttribute("class","number")
+    timestamp.setAttribute("class","time-stamp")
+
+    number.innerHTML=`#${++lapItem}`
+    timestamp.innerHTML=`${minCounter} : ${secCounter} : ${centiCounter}`
+    li.append(number,timestamp)
+    laps.append(li)
+
+    clearButton.classList.remove("hidden")
+}
+
+const clearAll = ()=>{
+    laps.innerHTML=''
+    clearButton.classList.add("hidden")
+    laps.append(clearButton)
+    lapItem=0
+}
+
+playButton.addEventListener("click",play)
+lapButton.addEventListener("click",lap)
+resetButton.addEventListener("click", reset)
+clearButton.addEventListener("click", clearAll)
